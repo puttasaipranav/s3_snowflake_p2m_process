5faf75ecc09774347138e767f72bd227be49d705
sai pranav <saipranav@sai-ka-MacBook-Air.local>
2024-02-03 22:11:44 +0530

    I have added a new txt file and changes the lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -2,5 +2,5 @@ from src.snowflake_s3_connection import connect_to_snowflake,snowflake_cursor
 import json
 
 def lambda_handler(event, context): 
-
+    print('hi')
     snowflake = snowflake_cursor()
